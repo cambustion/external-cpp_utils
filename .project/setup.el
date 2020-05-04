46dbc28e9472d645622fea5e27b68612f522a485
ramblehead <v.rybynok@gmail.com>
2020-05-04 16:49:59 +0100

    Guarding project setup() with buffer-file-name

--- .project/setup.el
+++ .project/setup.el
@@ -1,13 +1,14 @@
 ;; -*- coding: utf-8 -*-
 
-(let* ((project-root (rh-project-get-root))
-       (project-path (rh-project-get-path))
-       file-rpath)
-  (when project-root
-    (setq file-rpath (file-relative-name buffer-file-name project-root))
-    (cond
-     ((string-match-p "\\.css\\'" file-rpath)
-      (rh-setup-css-skewer))
-     ((eq major-mode 'c++-mode)
-      (set (make-local-variable 'compile-command)
-           (concat project-path "make"))))))
+(when buffer-file-name
+  (let* ((project-root (rh-project-get-root))
+         (project-path (rh-project-get-path))
+         file-rpath)
+    (when project-root
+      (setq file-rpath (file-relative-name buffer-file-name project-root))
+      (cond
+       ((string-match-p "\\.css\\'" file-rpath)
+        (rh-setup-css-skewer))
+       ((eq major-mode 'c++-mode)
+        (set (make-local-variable 'compile-command)
+             (concat project-path "make")))))))
